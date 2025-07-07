e12b96e7dc59848829f92906dc413e2dbb298ebe
Patel Rishikumar <153611518+rishiprp@users.noreply.github.com>
2025-07-07 10:16:54 +0530

    Create maxbetween2.cpp

---
+++ maxbetween2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+    int a,b;
+    std::cout<<"Enter value of A :";
+    std::cin>>a;
+    std::cout<<"Enter value of B :";
+    std::cin>>b;
+    
+    if(a>b){
+        std::cout<<a;   
+    }
+    else{
+        std::cout<<b;
+    }
+    
+
+    return 0;
+}
