95c0011ded173ec565ab209ae9c0a29746e11959
Patel Rishikumar <153611518+rishiprp@users.noreply.github.com>
2025-07-07 10:13:41 +0530

    Create Sumoftonumber.cpp

---
+++ Sumoftonumber.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+int main(){
+    int num1=15;
+    int num2=20;
+    int sum=num1+num2;
+    
+   std::cout<<"The sum of "<<num1<<"and"<<num2<<"is:"<<std::endl;
+    return 0;
+}
